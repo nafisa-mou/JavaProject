2626889a87652031571cb60d01d149598c369f99
nafisa-mou <2401014@uftb.ac.bd>
2026-05-26 14:20:52 +0600

    feat: Update AuthDTO and remove unused exception constructors; add new DTOs for password management

--- BloodLink/src/main/java/com/bloodlink/exception/BloodLinkExceptions.java
+++ BloodLink/src/main/java/com/bloodlink/exception/BloodLinkExceptions.java
@@ -1,10 +1,2 @@
 
 
-    public ValidationException(String message) {
-        super(message);
-    }
-
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
